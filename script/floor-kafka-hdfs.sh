9f372d582470c6950a3d79aa76241caba0ae9812
alan <awalan1605@163.com>
2022-06-10 10:24:36 +0800

    edited english comment.

--- script/floor-kafka-hdfs.sh
+++ script/floor-kafka-hdfs.sh
@@ -3,12 +3,12 @@
 case $1 in
 "start"){
 
-                echo " --------启动flume-------" 
+                echo " --------Start Flume-------" 
                 nohup flume-ng agent --conf-file /opt/script/flume/conf/floor-kafka-hdfs.conf --name os1 -Dflume.root.logger=INFO,LOGFILE > /opt/script/flume/log/floor-kafka-hdfs-log.txt   2>&1 &
 };;
 "stop"){
 
-                echo " --------停止flume-------" 
+                echo " --------Stop Flume-------" 
                 ps -ef | grep floor-kafka-hdfs.conf | grep -v grep |awk '{print $2}' | xargs -n1 kill
 
 };;
